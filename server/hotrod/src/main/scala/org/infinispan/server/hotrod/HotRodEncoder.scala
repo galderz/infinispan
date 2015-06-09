7014c87113403fab3242338fb5847e9f0acb502e
Galder Zamarreño <galder@zamarreno.com>
2015-06-09 09:20:44 +0200

    ISPN-5523 Use MessageToByteEncoder to avoid ByteBuf leak w/ exceptions

--- server/hotrod/src/main/scala/org/infinispan/server/hotrod/HotRodEncoder.scala
+++ server/hotrod/src/main/scala/org/infinispan/server/hotrod/HotRodEncoder.scala
@@ -1,9 +1,10 @@
 package org.infinispan.server.hotrod
 
+import io.netty.buffer.ByteBuf
 import logging.Log
 import org.infinispan.manager.EmbeddedCacheManager
 import org.infinispan.commons.util.Util
-import io.netty.handler.codec.MessageToMessageEncoder
+import io.netty.handler.codec.MessageToByteEncoder
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelHandler.Sharable
 import org.infinispan.server.hotrod.Events.Event
@@ -17,17 +18,16 @@ import org.infinispan.server.hotrod.OperationStatus._
  */
 @Sharable
 class HotRodEncoder(cacheManager: EmbeddedCacheManager, server: HotRodServer)
-        extends MessageToMessageEncoder[Any] with Constants with Log {
+        extends MessageToByteEncoder[Any] with Constants with Log {
 
    private lazy val isClustered: Boolean = cacheManager.getCacheManagerConfiguration.transport.transport != null
    private lazy val addressCache: AddressCache =
       if (isClustered) cacheManager.getCache(server.getConfiguration.topologyCacheName) else null
    private val isTrace = isTraceEnabled
 
-   def encode(ctx: ChannelHandlerContext, msg: Any, out: java.util.List[AnyRef]): Unit = {
+   def encode(ctx: ChannelHandlerContext, msg: Any, buf: ByteBuf): Unit = {
       trace("Encode msg %s", msg)
 
-      val buf = ctx.alloc().buffer
       msg match {
          case r: Response =>
             val encoder = getEncoder(r.version)
@@ -58,8 +58,6 @@ class HotRodEncoder(cacheManager: EmbeddedCacheManager, server: HotRodServer)
       if (isTrace)
          trace("Write buffer contents %s to channel %s",
             Util.hexDump(buf.nioBuffer), ctx.channel)
-
-      out.add(buf)
    }
 
    private def getEncoder(version: Byte): AbstractVersionedEncoder = {
