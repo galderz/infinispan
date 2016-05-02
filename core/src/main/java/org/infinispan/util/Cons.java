32034d18a896627e90b59260310f96d3abbf06aa
Dan Berindei <dan@infinispan.org>
2016-05-02 11:20:14 +0300

    Cons

---
+++ core/src/main/java/org/infinispan/util/Cons.java
@@ -0,0 +1,124 @@
+package org.infinispan.util;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+/**
+ * Node in an immutable single-linked list.
+ *
+ * @author Dan Berindei
+ * @since 9.0
+ */
+public class Cons<T> extends AbstractCollection<T> {
+   private static final Cons EMPTY = new Cons<Object>(null, null);
+   private final T head;
+   private final Cons<T> tail;
+
+   @SuppressWarnings("unchecked")
+   public static <T> Cons<T> empty() {
+      return EMPTY;
+   }
+
+   public static <T> Cons<T> make(T head, Cons<T> tail) {
+      return new Cons<T>(Objects.requireNonNull(head), Objects.requireNonNull(tail));
+   }
+
+   private Cons(T head, Cons<T> tail) {
+      this.head = head;
+      this.tail = tail;
+   }
+
+   public T head() {
+      if (this.isEmpty())
+         throw new IllegalStateException("empty");
+      return head;
+   }
+
+   public Cons<T> tail() {
+      if (this.isEmpty())
+         throw new IllegalStateException("empty");
+      return tail;
+   }
+
+   @Override
+   public boolean isEmpty() {
+      return this == EMPTY;
+   }
+
+   @Override
+   public Iterator<T> iterator() {
+      return new Iterator<T>() {
+         Cons<T> current = Cons.this;
+
+         @Override
+         public boolean hasNext() {
+            return current != EMPTY;
+         }
+
+         @Override
+         public T next() {
+            if (current == EMPTY)
+               throw new NoSuchElementException();
+
+            T value = current.head;
+            current = current.tail;
+            return value;
+         }
+      };
+   }
+
+   @Override
+   public int size() {
+      int size = 0;
+      Cons<T> current = this;
+      while (current != EMPTY) {
+         size++;
+         current = current.tail;
+      }
+      return size;
+   }
+
+   @Override
+   public void forEach(Consumer<? super T> action) {
+      for (Cons<T> current = this; current != EMPTY; current = current.tail) {
+         action.accept(current.head);
+      }
+   }
+
+   @Override
+   public Spliterator<T> spliterator() {
+      return new Spliterator<T>() {
+         Cons<T> current = Cons.this;
+
+         @Override
+         public boolean tryAdvance(Consumer<? super T> action) {
+            Objects.requireNonNull(action);
+            if (current == EMPTY)
+               return false;
+
+            action.accept(current.head);
+            current = current.tail;
+            return true;
+         }
+
+         @Override
+         public Spliterator<T> trySplit() {
+            return null;
+         }
+
+         @Override
+         public long estimateSize() {
+            return Long.MAX_VALUE;
+         }
+
+         @Override
+         public int characteristics() {
+            return ORDERED | IMMUTABLE;
+         }
+      };
+   }
+}
